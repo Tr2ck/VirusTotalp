45e5dc9d20ccdec07a86e1087741108a597e762d
Tr2ck <41378495+Tr2ck@users.noreply.github.com>
2020-02-22 15:43:31 +0800

    Create ip_domain.py

---
+++ ip_domain.py
@@ -0,0 +1,33 @@
+import requests
+import json
+
+ip="127.0.0."
+filename = "log.txt"
+
+def get(number):
+	try:
+		url = "https://www.virustotal.com/vtapi/v2/ip-address/report?apikey=<APIkey>&ip="+str(ip)+str(number)
+		print url
+		headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36'}
+		response = requests.get(url,headers=headers)
+		jsonstr = json.loads(response.text)     
+		hostname = jsonstr['resolutions']   #打印反查结果
+		print hostname
+		f = open(filename,'a')
+		f.write(json.dumps(hostname,ensure_ascii=False))
+		f.write('\n')
+		f.close()
+	except:
+		pass
+
+
+
+
+def main():
+	for number in range(62,64):
+		get(number)
+
+
+
+if __name__ == "__main__":
+	main()
