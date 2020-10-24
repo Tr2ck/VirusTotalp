a2e9c98a5135a3bc3b5d3533b725589fea4c6d93
Tr2ck <41378495+Tr2ck@users.noreply.github.com>
2020-10-24 09:08:37 +0800

    Update subdomain.py
    添加header头

--- subdomain.py
+++ subdomain.py
@@ -1,4 +1,3 @@
-
 import requests
 import time
 import json
@@ -14,13 +13,16 @@ def get(number):
         try:
                 cursor="I"+str(number*10)+"\n."
                 bs64 = base64.b64encode(cursor)
+                header = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.80 Safari/537.36"}
                 url = "https://www.virustotal.com/ui/domains/"+domains+"/subdomains?relationships=resolutions&cursor="+str(bs64)+"&limit=10"
                 print url
-                res = requests.get(url)
+                res = requests.get(url,headers=header)
                 t = json.loads(res.text)
+                print t
                 if t.has_key('error') != 'Ture':
                         for x in range(10):
                                 print t['data'][x]['id']
+
         except:
                 pass
 
